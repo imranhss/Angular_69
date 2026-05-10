6a81e11b178e364cf492b6f5ecdf11a642841c8f
Muhammad Emran Hossain <emranhss@gmail.com>
2026-05-10 18:10:47 +0600

    add

--- JEE_69_angular/src/app/component/layout/header/header.ts
+++ JEE_69_angular/src/app/component/layout/header/header.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {}
+export class Header implements OnInit {
+
+
+  loggedUser: any;
+  constructor(
+    private cdr: ChangeDetectorRef
+  ){}
+
+  ngOnInit(): void {
+    this.loadRole();
+     this.cdr.markForCheck();
+  }
+
+  loadRole() {
+
+    const userData = localStorage.getItem('user');
+    this.loggedUser = JSON.parse(userData!);
+   
+    console.log(this.loggedUser);
+  }
+
+
+
+
+
+
+
+}
